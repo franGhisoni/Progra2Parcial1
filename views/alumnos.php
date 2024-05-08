93fe4ff163e8bb3d667bccde1f6e67d587a5ab2f
Facundo <facundo.paez@davinci.edu.ar>
2024-05-08 20:29:01 -0300

    Staff + CSS actualizado

---
+++ views/alumnos.php
@@ -0,0 +1,39 @@
+<div class="alumnos">
+    <div class="row">
+        <div class="col">
+            <div class="our-team">
+                <div class="pic">
+                    <img src="img/1.jpg">
+                    <ul class="social">
+                        <li><a href="https://www.instagram.com/facupaez_97/" target="_blank" class="fab fa-instagram"></a></li>
+                        <li><a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=facundo.paez@davinci.edu.ar" target="_blank" class="fab fa-at"></a></li>
+                    </ul>
+                </div>
+                <div class="team-content">
+                    <div class="team-info">
+                        <h3 class="title">Facundo Paez</h3>
+                        <span class="post">8/3/1997</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+        <div class="col">
+            <div class="our-team">
+                <div class="pic">
+                <img src="img/2.jpg">
+                    <ul class="social">
+                    <li><a href="https://www.instagram.com/otroalienn/" target="_blank" class="fab fa-instagram"></a></li>
+                    <li><a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=florencia.sueiro@davinci.edu.ar" target="_blank" class="fab fa-at"></a></li>
+                    </ul>
+                </div>
+                <div class="team-content">
+                    <div class="team-info">
+                        <h3 class="title">Florencia Sueiro</h3>
+                        <span class="post">24/8/1999</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
