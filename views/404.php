85002790348eb6788c42107849f9862f45eee386
Facundo <facundo.paez@davinci.edu.ar>
2024-05-11 02:01:18 -0300

    404 design

---
+++ views/404.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="error-text">404</div>
+    <div class="message">Lo sentimos, pero no encontramos ese sitio.</div>
+    <div class="boton404">
+    <a href="index.php?sec=home" class="button404">Ir al inicio</a></div>
+  </div>
