d2205226fed5a1ff1f927621b4c07865a35b8208
Facundo <facundo.paez@davinci.edu.ar>
2024-05-11 02:38:03 -0300

    404 funcionando de perlas

--- index.php
+++ index.php
@@ -27,9 +27,11 @@ $seccion = isset($_GET['sec']) ? $_GET['sec'] : 'home';
 if (!array_key_exists($seccion, $secciones_validas)) {
     $vista = "404";
     $titulo = "404: Página no encontrada";
+    $body_class = 'body-404';
 } else {
     $vista = $seccion;
     $titulo = $secciones_validas[$seccion]['titulo'];
+    $body_class = "body-$seccion";
 }
 
 ?>
@@ -44,7 +46,7 @@ if (!array_key_exists($seccion, $secciones_validas)) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/estilos.css">
 </head>
-<body class="body-<?= $seccion ?>">
+<body class="<?= $body_class ?>">
 
     <main class="main">
         <section class="main">
